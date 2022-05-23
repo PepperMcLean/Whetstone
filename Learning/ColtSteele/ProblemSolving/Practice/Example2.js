7886a9a4e6c0b569ad5432d2b5f8de54c086fa65
PepperMcLean <jackmcl206@gmail.com>
2022-05-23 16:16:43 -0500

    Problem solving, Break It Down

--- Learning/ColtSteele/ProblemSolving/Practice/Example2.js
+++ Learning/ColtSteele/ProblemSolving/Practice/Example2.js
@@ -7,20 +7,29 @@ charCount("hello"); // {h:1, e:1, l:2, o:1}
 // good questions should ask about casing, numbers, symbols, expected output
 // depending on edgecases what should you do
 
+// function charCount(str) {
+  // make object to return at end
+  // loop over string, for each character...
+    // if the char is number/letter AND a key in object, add one to count
+    // if the char is number/letter AND is not in our object, add it and set value to 1
+    // if the char is something else, i.e. $, don't do anything
+  // return object at end
+// }
+
+
+
 function charCount(str) {
-  let banned = ['$','0']
-  let arr = str.toLowerCase().split('');
-  let obj = {}
-  arr.forEach(element => {
-    if (obj[element] = true) {
-      obj[element] += 1;
-    } else {
-      obj[element] = 1;
-    }
-    if (banned.includes(element)) { 
-      obj = "error invalid char"
-      return;
-    }
-  });
+  let obj = {}// make object to return at end
+  let arr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+    "s","t","u","v","w","x","y","z","1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]// create acceptable inputs
+  let newStr = str// downcase the string
+  for (let i = 0; i < newStr.length; i++) {// loop over string, for each character...
+    if (arr.includes(newStr[i]) &&  obj[newStr[i]]) // if the char is number/letter AND a key in object, add one to count
+      obj[newStr[i]] += 1;
+    else if (arr.includes(newStr[i]) && !obj[newStr[i]])  // if the char is number/letter AND is not in our object, add it and set value to 1
+      obj[newStr[i]] = 1;
+    else {} // if the char is something else, i.e. $, don't do anything
+  }
   console.log(obj)
+  return obj;// return object at end
 }
